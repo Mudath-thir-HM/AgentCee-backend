1555c8d3b94a259840f5a15cdc554c09e243c77f
Mudath-thir-HM <eighthmudathir@gmail.com>
2026-05-24 19:51:47 +0100

    temporary cors allowance

--- app/core/cors.py
+++ app/core/cors.py
@@ -5,9 +5,7 @@ from app.core.config import settings
 def setup_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            settings.FRONTEND_URL
-        ],
+        allow_origins=["*"], # Temporarily allow everything
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
