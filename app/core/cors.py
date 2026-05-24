98ce915862f0a34d643db5f14cd21cdf7ad03b09
Mudath-thir-HM <eighthmudathir@gmail.com>
2026-05-24 20:25:59 +0100

    cors testing 5

--- app/core/cors.py
+++ app/core/cors.py
@@ -6,7 +6,7 @@ def setup_cors(app):
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
-            settings.FRONTEND_URL or "https://agentcee.netlify.app"
+            "https://agentcee.netlify.app"
         ],
         allow_credentials=True,
         allow_methods=["*"],
